f7564d86842e60f8cbb402a884a316524625522b
srinathzeesoft <srinathzeesoft@gmail.com>
2026-08-13 17:31:25 +0530

    Update mkt3.0_messages.js

--- mkt3.0_messages.js
+++ mkt3.0_messages.js
@@ -1,5 +1,5 @@
 Mkt3L10n.adminFieldHtmlEncodeForm = {
-  Html_Encode_Tokens: "Tokens Checking"
+  Html_Encode_Tokens: "Tokens Checking now checking checking checking"
   Explain: "Configurar o comportamento padrão de codificação HTML em tokens de e-mail para todos os campos checking",
   Remind: "Esta definição se aplica a todos os campos. Você também pode configurar esta definição para cada campo test test test test test test test",
   Save: "SALVAR",
